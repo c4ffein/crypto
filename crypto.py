6344ae3269fb579937f8a35398c911d6c8ebc69f
c4ffein <c4ffein@gmail.com>
2025-04-28 23:54:48 +0200

    skeleton

---
+++ crypto.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+crypto - crypto tools
+MIT License - Copyright (c) 2025 c4ffein
+WARNING: I don't recommand using this as-is. This a PoC, and usable by me because I know what I want to do with it.
+- You can use it if you feel that you can edit the code yourself and you can live with my future breaking changes.
+"""
+
+
+class CryptoCliException(Exception):
+    pass
+
+
+def main():
+    return -4  # TODO
+
+
+if __name__ == "__main__":
+    try:
+        exit(main())
+    except KeyboardInterrupt:
+        print("\n  !!  KeyboardInterrupt received  !!  \n")
+        exit(-2)
+    except CryptoCliException as e:
+        print(f"{Color.RED.value}\n  !!  {e}  !!  \n")
+        exit(-1)
+    except Exception:
+        raise
